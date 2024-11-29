31bc810b56b0414b9ef61cd8b7e24c5e8e29670d
SourceSenseiTheRealOne <source.sensei@proton.me>
2024-11-29 23:17:31 +0000

    ui quick update

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -5,28 +5,52 @@ const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        <span>
-          <Heading
-            level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
-          >
-            Well done! You have successfully deployed your Medusa 2.0 store on Railway!
-          </Heading>
-          <Heading
-            level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
-          >
-            Need help customizing your store?
-          </Heading>
-        </span>
-        <a
-          href="https://funkyton.com/medusajs-2-0-is-finally-here/"
-          target="_blank"
-        >
-          <h1 style={{ textDecoration: "underline" }}>
-            Visit the tutorial
-          </h1>
-        </a>
+        <section className="text-gray-600 body-font">
+          <div className="container px-5 py-24 mx-auto flex flex-wrap">
+            <div className="flex flex-wrap -mx-4 mt-auto mb-auto lg:w-1/2 sm:w-2/3 content-start sm:pr-10">
+              <div className="w-full sm:p-4 px-4 mb-6">
+                <h1 className="title-font font-medium text-xl mb-2 text-gray-900">
+                  Moon hashtag pop-up try-hard offal truffaut
+                </h1>
+                <div className="leading-relaxed">
+                  Pour-over craft beer pug drinking vinegar live-edge gastropub,
+                  keytar neutra sustainable fingerstache kickstarter.
+                </div>
+              </div>
+              <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
+                <h2 className="title-font font-medium text-3xl text-gray-900">
+                  2.7K
+                </h2>
+                <p className="leading-relaxed">Users</p>
+              </div>
+              <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
+                <h2 className="title-font font-medium text-3xl text-gray-900">
+                  1.8K
+                </h2>
+                <p className="leading-relaxed">Subscribes</p>
+              </div>
+              <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
+                <h2 className="title-font font-medium text-3xl text-gray-900">
+                  35
+                </h2>
+                <p className="leading-relaxed">Downloads</p>
+              </div>
+              <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
+                <h2 className="title-font font-medium text-3xl text-gray-900">
+                  4
+                </h2>
+                <p className="leading-relaxed">Products</p>
+              </div>
+            </div>
+            <div className="lg:w-1/2 sm:w-1/3 w-full rounded-lg overflow-hidden mt-6 sm:mt-0">
+              <img
+                className="object-cover object-center w-full h-full"
+                src="https://dummyimage.com/600x300"
+                alt="Statistics visualization"
+              />
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   )
